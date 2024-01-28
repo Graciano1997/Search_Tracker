2324dbfb869ff63f403e08f657c1a2a547e8037b
Graciano1997 <gracianomanuelhenrique@gmail.com>
2024-01-28 13:13:04 +0100

    remove the console logs

--- app/javascript/modules/searchHandler.js
+++ app/javascript/modules/searchHandler.js
@@ -5,11 +5,9 @@ function searchHandler() {
 
     if (document.querySelector('#searchBox') !== null) {
         document.querySelector('#searchBox').addEventListener('input', async (el) => {
-            console.log(el.target.value);
             clearTimeout(typingTimer);
 
             typingTimer = setTimeout(async () => {
-                console.log('stop type');
                 historic = el.target.value;
                 if (historic !== '') {
                     await fetch('/analytic_trends', {
@@ -20,7 +18,7 @@ function searchHandler() {
                         body: JSON.stringify({ query: historic.toLowerCase() }),
                     })
                         .then((response) => {
-                            console.log(response.json());
+                            return response.json();
                         })
                 }
             }, typingInterval);
